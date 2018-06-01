71d6144ff62ddd4cc1d5ebbef0865a0df258d38c
Ruslan Bukin <br@bsdpad.com>
2018-06-01 20:17:37 +0100

    add gpio driver template

---
+++ sys/arm/stm/stm32f4_gpio.c
@@ -0,0 +1,97 @@
+/*-
+ * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <arm/stm/stm32f4_gpio.h>
+
+#define	RD4(_sc, _reg)		*(volatile uint32_t *)((_sc)->base + _reg)
+#define	WR4(_sc, _reg, _val)	*(volatile uint32_t *)((_sc)->base + _reg) = _val
+
+#if 0
+void
+pin_set(struct stm32f4_gpio_softc *sc, uint32_t port,
+    uint32_t pin, uint32_t enable)
+{
+	uint32_t reg;
+
+	if (enable)
+		reg = (1 << pin);
+	else
+		reg = (1 << (pin + 16));
+
+	WR4(sc, GPIO_BSSR(port), reg);
+}
+#endif
+
+void
+pin_configure(struct stm32f4_gpio_softc *sc, struct gpio_pin *pins)
+{
+	struct gpio_pin *cfg;
+	uint32_t afr;
+	uint32_t reg;
+	int i;
+
+	for (i = 0; pins[i].pin != -1; i++) {
+		cfg = &pins[i];
+
+		reg = RD4(sc, GPIO_MODER(cfg->port));
+		reg &= ~(0x3 << (cfg->pin * 2));
+		reg |= (cfg->mode << (cfg->pin * 2));
+		WR4(sc, GPIO_MODER(cfg->port), reg);
+
+		if (cfg->mode == MODE_ALT) {
+			if (cfg->pin > 7)
+				afr = GPIO_AFRH(cfg->port);
+			else
+				afr = GPIO_AFRL(cfg->port);
+
+			reg = RD4(sc, afr);
+			reg &= ~(0xf << ((cfg->pin % 8) * 4));
+			reg |= (cfg->alt << ((cfg->pin % 8) * 4));
+			WR4(sc, afr, reg);
+		}
+
+		reg = RD4(sc, GPIO_OSPEEDR(cfg->port));
+		reg &= ~(3 << (cfg->pin * 2));
+		reg |= (3 << (cfg->pin * 2));
+		WR4(sc, GPIO_OSPEEDR(cfg->port), reg);
+
+		reg = RD4(sc, GPIO_PUPDR(cfg->port));
+		reg &= ~(3 << (cfg->pin * 2));
+		reg |= (cfg->pupdr << (cfg->pin * 2));
+		WR4(sc, GPIO_PUPDR(cfg->port), reg);
+	}
+}
+
+int
+stm32f4_gpio_init(struct stm32f4_gpio_softc *sc, uint32_t base,
+    uint32_t cpu_freq, uint32_t baud_rate)
+{
+
+	sc->base = base;
+
+	return (0);
+}
---
+++ sys/arm/stm/stm32f4_gpio.h
@@ -0,0 +1,76 @@
+/*-
+ * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _ARM_STM_STM32F4_GPIO_H_
+#define _ARM_STM_STM32F4_GPIO_H_
+
+#define	PORT_A	0
+#define	PORT_B	1
+#define	PORT_C	2
+#define	PORT_D	3
+#define	PORT_E	4
+#define	PORT_F	5
+#define	PORT_G	6
+#define	PORT_H	7
+#define	PORT_I	8
+#define	PORT_J	9
+#define	PORT_K	10
+
+#define	GPIO_MODER(n)	(0x00 + 0x400 * n)
+#define	GPIO_OTYPER(n)	(0x04 + 0x400 * n)
+#define	GPIO_OSPEEDR(n)	(0x08 + 0x400 * n)
+#define	GPIO_BSSR(n)	(0x18 + 0x400 * n)
+#define	GPIO_AFRL(n)	(0x20 + 0x400 * n)
+#define	GPIO_AFRH(n)	(0x24 + 0x400 * n)
+#define	GPIO_PUPDR(n)	(0x0C + 0x400 * n)
+
+#define	MODE_INP	0
+#define	MODE_OUT	1
+#define	MODE_ALT	2
+#define	MODE_ANA	3
+
+#define	FLOAT		0
+#define	PULLUP		1
+#define	PULLDOWN	2
+
+struct gpio_pin {
+	uint32_t	port;
+	uint32_t	pin;
+	uint32_t	mode;
+	uint32_t	alt;
+	uint32_t	pupdr;
+};
+
+struct stm32f4_gpio_softc {
+	uint32_t base;
+};
+
+int stm32f4_gpio_init(struct stm32f4_gpio_softc *sc, uint32_t base,
+    uint32_t cpu_freq, uint32_t baud_rate);
+
+void pin_configure(struct stm32f4_gpio_softc *sc, struct gpio_pin *pins);
+
+#endif /* !_ARM_STM_STM32F4_GPIO_H_ */
