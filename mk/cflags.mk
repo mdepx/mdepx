3e719c394df82907cd1aed1fb8002f6b59cd2559
Ruslan Bukin <br@bsdpad.com>
2018-06-18 11:45:04 +0100

    move stddef and stdint to include/

--- mk/cflags.mk
+++ mk/cflags.mk
@@ -1,6 +1,7 @@
 CFLAGS += ${INCS} ${CFLAGS_$(.TARGET)} -D__OSFIVE__
 
 INCS += -I${.CURDIR}
+INCS += -I${.CURDIR}/osfive/include
 INCS += -I${.CURDIR}/osfive/sys
 INCS += -I${.CURDIR}/osfive/lib
 INCS += -I${.CURDIR}/osfive/lib/libc/include
