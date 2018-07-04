badb954909e6a2ccb9435636feda36938c32a8e5
Ruslan Bukin <br@bsdpad.com>
2018-07-04 13:26:34 +0100

    add bcmp

--- include/strings.h
+++ include/strings.h
@@ -29,6 +29,7 @@
 
 void bzero(void *b, size_t len);
 void bcopy(const void *src, void *dst, size_t len);
+int bcmp(const void *b1, const void *b2, size_t len);
 int strcasecmp(const char *s1, const char *s2);
 int strncasecmp(const char *s1, const char *s2, size_t len);
 int ffs(int value);
