85dfc1a473b36ee60a20e19e746d77e82bc81b1b
Ruslan Bukin <br@bsdpad.com>
2018-07-09 14:35:45 +0100

    Add BERI EPW (Emulated Peripheral Window) registers

---
+++ sys/mips/beri/beri_epw.h
@@ -0,0 +1,47 @@
+/*-
+ * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_MIPS_BERI_BERI_EPW_
+#define	_MIPS_BERI_BERI_EPW_
+
+#define	EPW_READ_ADDRESS		0x0000	/* read only */
+#define	EPW_READ_FLIT_SIZE		0x0008	/* read only */
+#define	EPW_READ_BURST_COUNT		0x000C	/* read only */
+#define	EPW_READ_RESPONSE_DATA		0x0040	/* read/write */
+#define	EPW_WRITE_ADDRESS		0x1000	/* read only */
+#define	EPW_WRITE_BYTE_ENABLE		0x1008	/* read only */
+#define	EPW_WRITE_DATA			0x1040	/* read only */
+#define	EPW_TIME_STAMP			0x2000	/* read only */
+#define	EPW_REQUEST_ID			0x2004	/* read only */
+#define	EPW_REQUEST_IS_WRITE		0x2006	/* read only */
+#define	EPW_REQUEST_LEVEL_SEND_RESPONSE	0x2007	/* read/write */
+#define	EPW_ENABLE_DEVICE_EMULATION	0x2008	/* read/write */
+
+#endif /* !_MIPS_BERI_BERI_EPW_ */
