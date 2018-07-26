fa2d7d405ac15bc2a4d7a6fb94157b41a4fb0da0
Ruslan Bukin <br@bsdpad.com>
2018-07-26 21:29:37 +0100

    add registers

--- sys/arm/stm/stm32f4_gpio.h
+++ sys/arm/stm/stm32f4_gpio.h
@@ -30,10 +30,14 @@
 #define	GPIO_MODER(n)	(0x00 + 0x400 * (n))
 #define	GPIO_OTYPER(n)	(0x04 + 0x400 * (n))
 #define	GPIO_OSPEEDR(n)	(0x08 + 0x400 * (n))
+#define	GPIO_PUPDR(n)	(0x0C + 0x400 * (n))
+#define	GPIO_IDR(n)	(0x10 + 0x400 * (n))
+#define	GPIO_ODR(n)	(0x14 + 0x400 * (n))
 #define	GPIO_BSSR(n)	(0x18 + 0x400 * (n))
+#define	GPIO_LCKR(n)	(0x1C + 0x400 * (n))
 #define	GPIO_AFRL(n)	(0x20 + 0x400 * (n))
 #define	GPIO_AFRH(n)	(0x24 + 0x400 * (n))
-#define	GPIO_PUPDR(n)	(0x0C + 0x400 * (n))
+#define	GPIO_BRR(n)	(0x28 + 0x400 * (n))
 
 #define	MODE_INP	0
 #define	MODE_OUT	1
