51f6b71016d3051e18476f12f102cd44eab83562
Ruslan Bukin <br@bsdpad.com>
2018-08-21 21:50:45 +0100

    Read out the mac address

--- sys/arm/stm/stm32f7_eth.c
+++ sys/arm/stm/stm32f7_eth.c
@@ -25,15 +25,59 @@
  */
 
 #include <sys/cdefs.h>
+#include <net/ethernet.h>
 
 #include "stm32f7_eth.h"
 
 #define	RD4(_sc, _reg)		*(volatile uint32_t *)((_sc)->base + _reg)
 #define	WR4(_sc, _reg, _val)	*(volatile uint32_t *)((_sc)->base + _reg) = _val
 
+static void
+dwc_get_hwaddr(struct stm32f7_eth_softc *sc, uint8_t *hwaddr)
+{
+	uint32_t hi, lo;
+
+	lo = RD4(sc, ETH_MACALR(0));
+	hi = RD4(sc, ETH_MACAHR(0)) & 0xffff;
+
+	if ((lo != 0xffffffff) || (hi != 0xffff)) {
+		hwaddr[0] = (lo >>  0) & 0xff;
+		hwaddr[1] = (lo >>  8) & 0xff;
+		hwaddr[2] = (lo >> 16) & 0xff;
+		hwaddr[3] = (lo >> 24) & 0xff;
+		hwaddr[4] = (hi >>  0) & 0xff;
+		hwaddr[5] = (hi >>  8) & 0xff;
+	} else {
+		hwaddr[0] = 'o';
+		hwaddr[1] = 's';
+		hwaddr[2] = 'f';
+		hwaddr[3] = 'i';
+		hwaddr[4] = 'v';
+		hwaddr[5] = 'e';
+	}
+}
+
+static void
+dwc_set_hwaddr(struct stm32f7_eth_softc *sc, uint8_t *hwaddr)
+{
+	uint32_t hi, lo;
+
+	lo = hwaddr[0] | (hwaddr[1] << 8) | (hwaddr[2] << 16) |
+	    (hwaddr[3] << 24);
+	hi = hwaddr[4] | (hwaddr[5] << 8);
+
+	WR4(sc, ETH_MACALR(0), lo);
+	WR4(sc, ETH_MACAHR(0), hi);
+	WR4(sc, ETH_MACFFR, (MACFFR_PM | MACFFR_RA));
+}
+
 void
 stm32f7_eth_init(struct stm32f7_eth_softc *sc, uint32_t base)
 {
+	uint8_t hwaddr[ETHER_ADDR_LEN];
 
 	sc->base = base;
+
+	dwc_get_hwaddr(sc, hwaddr);
+	dwc_set_hwaddr(sc, hwaddr);
 }
