008c2ac61cb83abb014161c19f45dfd90834f788
Ruslan Bukin <br@bsdpad.com>
2018-10-09 11:06:42 +0100

    add some registers

--- sys/arm/stm/stm32l4_adc.h
+++ sys/arm/stm/stm32l4_adc.h
@@ -30,7 +30,25 @@
 #define	ADC_ISR		0x00 /* ADC interrupt and status register */
 #define	ADC_IER		0x04 /* ADC interrupt enable register */
 #define	ADC_CR		0x08 /* ADC control register */
+#define	 CR_ADCAL	(1 << 31) /* ADC calibration */
+#define	 CR_ADCALDIF	(1 << 30) /* Differential mode for calibration */
+#define	 CR_DEEPPWD	(1 << 29) /* Deep-power-down enable */
+#define	 CR_ADVREGEN	(1 << 28) /* ADC voltage regulator enable */
+#define	 CR_JADSTP	(1 << 5) /* ADC stop of injected conversion command */
+#define	 CR_ADSTP	(1 << 4) /* ADC stop of regular conversion command */
+#define	 CR_JADSTART	(1 << 3) /* ADC start of injected conversion */
+#define	 CR_ADSTART	(1 << 2) /* ADC start of regular conversion */
+#define	 CR_ADDIS	(1 << 1) /* ADC disable command */
+#define	 CR_ADEN	(1 << 0) /* ADC enable control */
 #define	ADC_CFGR	0x0C /* ADC configuration register */
+#define	 CFGR_RES_S	3 /* Data resolution */
+#define	 CFGR_RES_M	(0x3 << CFGR_RES_S)
+#define	 CFGR_RES_12	(0 << CFGR_RES_S)
+#define	 CFGR_RES_10	(1 << CFGR_RES_S)
+#define	 CFGR_RES_8	(2 << CFGR_RES_S)
+#define	 CFGR_RES_6	(3 << CFGR_RES_S)
+#define	 CFGR_DMAEN	(1 << 0) /* Direct memory access enable */
+#define	 CFGR_DMACFG	(1 << 1) /* DMA Circular mode selected */
 #define	ADC_CFGR2	0x10 /* ADC configuration register 2 */
 #define	ADC_SMPR1	0x14 /* ADC sample time register 1 */
 #define	ADC_SMPR2	0x18 /* ADC sample time register 2 */
@@ -46,6 +64,9 @@
 #define	ADC_OFR(n)	(0x60 + 0x4 * ((n) - 1)) /* ADC offset register 1,2,3,4 */
 #define	ADC_JDR(n)	(0x80 + 0x4 * ((n) - 1)) /* ADC injected data register */
 #define	ADC_AWD2CR	0xA0 /* ADC Analog Watchdog 2 Configuration Register */
+#define	 AWD2CR_AWD2CH_S	0 /* Analog watchdog 2 channel selection */
+#define	 AWD2CR_AWD2CH_M	(0x7ffff << AWD2CR_AWD2CH_S)
+#define	 AWD2CR_AWD2CH(n)	((n) << AWD2CR_AWD2CH_S)
 #define	ADC_AWD3CR	0xA4 /* ADC Analog Watchdog 3 Configuration Register */
 #define	ADC_DIFSEL	0xB0 /* ADC Differential mode Selection Register */
 #define	ADC_CALFACT	0xB4 /* ADC Calibration Factors */
