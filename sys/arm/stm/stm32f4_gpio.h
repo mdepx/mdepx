e309f892ee0ce5baa7dcb50cba4b7e7a8713eda0
Ruslan Bukin <br@bsdpad.com>
2019-01-31 09:21:43 +0000

    trim comments

--- sys/arm/stm/stm32f4_gpio.h
+++ sys/arm/stm/stm32f4_gpio.h
@@ -27,26 +27,26 @@
 #ifndef _ARM_STM_STM32F4_GPIO_H_
 #define _ARM_STM_STM32F4_GPIO_H_
 
-#define	GPIO_MODER(n)	(0x00 + 0x400 * (n))	/* GPIO port mode register */
+#define	GPIO_MODER(n)	(0x00 + 0x400 * (n))	/* port mode */
 #define	 MODE_INP	0	/* Input mode */
 #define	 MODE_OUT	1	/* General purpose output mode */
 #define	 MODE_ALT	2	/* Alternate function mode */
 #define	 MODE_ANA	3	/* Analog mode (reset state) */
-#define	GPIO_OTYPER(n)	(0x04 + 0x400 * (n))	/* GPIO port output type register */
+#define	GPIO_OTYPER(n)	(0x04 + 0x400 * (n))	/* port output type */
 #define	 OT_PUSHPULL	0
 #define	 OT_OPENDRAIN	1
-#define	GPIO_OSPEEDR(n)	(0x08 + 0x400 * (n))	/* GPIO port output speed register */
-#define	GPIO_PUPDR(n)	(0x0C + 0x400 * (n))	/* GPIO port pull-up/pull-down register */
+#define	GPIO_OSPEEDR(n)	(0x08 + 0x400 * (n))	/* port output speed */
+#define	GPIO_PUPDR(n)	(0x0C + 0x400 * (n))	/* port pull-up/pull-down */
 #define	 FLOAT		0	/* No pull-up, pull-down */
 #define	 PULLUP		1
 #define	 PULLDOWN	2
-#define	GPIO_IDR(n)	(0x10 + 0x400 * (n))	/* GPIO port input data register */
-#define	GPIO_ODR(n)	(0x14 + 0x400 * (n))	/* GPIO port output data register */
-#define	GPIO_BSRR(n)	(0x18 + 0x400 * (n))	/* GPIO port bit set/reset register */
-#define	GPIO_LCKR(n)	(0x1C + 0x400 * (n))	/* GPIO port configuration lock register */
-#define	GPIO_AFRL(n)	(0x20 + 0x400 * (n))	/* GPIO alternate function low register */
-#define	GPIO_AFRH(n)	(0x24 + 0x400 * (n))	/* GPIO alternate function high register */
-#define	GPIO_BRR(n)	(0x28 + 0x400 * (n))	/* GPIO port bit reset register */
+#define	GPIO_IDR(n)	(0x10 + 0x400 * (n))	/* port input data */
+#define	GPIO_ODR(n)	(0x14 + 0x400 * (n))	/* port output data */
+#define	GPIO_BSRR(n)	(0x18 + 0x400 * (n))	/* port bit set/reset */
+#define	GPIO_LCKR(n)	(0x1C + 0x400 * (n))	/* port configuration lock */
+#define	GPIO_AFRL(n)	(0x20 + 0x400 * (n))	/* alternate function low */
+#define	GPIO_AFRH(n)	(0x24 + 0x400 * (n))	/* alternate function high */
+#define	GPIO_BRR(n)	(0x28 + 0x400 * (n))	/* port bit reset */
 
 #define	PINS_END	{ -1, -1, -1, -1, -1 }
 
