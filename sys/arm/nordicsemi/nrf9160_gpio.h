a5145b98ff8ec27bc214a4167348095bc4588209
Ruslan Bukin <br@bsdpad.com>
2019-02-01 21:20:33 +0000

    Add gpio configuration registers

--- sys/arm/nordicsemi/nrf9160_gpio.h
+++ sys/arm/nordicsemi/nrf9160_gpio.h
@@ -38,6 +38,28 @@
 #define	GPIO_DETECTMODE		0x024	/* Select between default DETECT signal behaviour and LDETECT mode (For non-secure pin only) */
 #define	GPIO_DETECTMODE_SEC	0x028	/* Select between default DETECT signal behaviour and LDETECT mode (For secure pin only) */
 #define	GPIO_PIN_CNF(n)		(0x200 + (n) * 0x4)	/* Configuration of GPIO pins */
+#define	 CNF_RW_DIR_OUT		(1 << 0) /* Configure pin as an output pin */
+#define	 CNF_RW_INPUT_DIS	(1 << 1) /* Disconnect input buffer */
+#define	 CNF_RW_PULL_S		2 /* Pull configuration */
+#define	 CNF_RW_PULL_M		(0x3 << CNF_RW_PULL_S)
+#define	 CNF_RW_PULL_DISABLED	(0x0 << CNF_RW_PULL_S)
+#define	 CNF_RW_PULL_DOWN	(0x1 << CNF_RW_PULL_S)
+#define	 CNF_RW_PULL_UP		(0x2 << CNF_RW_PULL_S)
+#define	 CNF_RW_DRIVE_S		8 /* Drive configuration */
+#define	 CNF_RW_DRIVE_M		(0x7 << CNF_RW_DRIVE_S)
+#define	 CNF_RW_DRIVE_S0S1	(0 << CNF_RW_DRIVE_S) /* Std 0, std 1 */
+#define	 CNF_RW_DRIVE_H0S1	(1 << CNF_RW_DRIVE_S) /* High drive 0, std 1 */
+#define	 CNF_RW_DRIVE_S0H1	(2 << CNF_RW_DRIVE_S) /* Std 0, high drive 1 */
+#define	 CNF_RW_DRIVE_H0H1	(3 << CNF_RW_DRIVE_S) /* High drive 0, 1 */
+#define	 CNF_RW_DRIVE_D0S1	(4 << CNF_RW_DRIVE_S) /* Disconnect 0, std 1 */
+#define	 CNF_RW_DRIVE_D0H1	(5 << CNF_RW_DRIVE_S) /* Dis 0, high drive 1 */
+#define	 CNF_RW_DRIVE_S0D1	(6 << CNF_RW_DRIVE_S) /* Std 0, disconnect 1 */
+#define	 CNF_RW_DRIVE_H0D1	(6 << CNF_RW_DRIVE_S) /* High drive 0, dis 1 */
+#define	 CNF_RW_SENSE_S		16 /* Pin sensing mechanism */
+#define	 CNF_RW_SENSE_M		(0x7 << CNF_RW_SENSE_S)
+#define	 CNF_RW_SENSE_DISABLED	(0 << CNF_RW_SENSE_S)
+#define	 CNF_RW_SENSE_HIGH	(2 << CNF_RW_SENSE_S)
+#define	 CNF_RW_SENSE_LOW	(3 << CNF_RW_SENSE_S)
 
 struct gpio_softc {
 	size_t base;
