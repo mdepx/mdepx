18835d7a7d7c721ca3368c9d4bc83ad0742d879b
Ruslan Bukin <br@bsdpad.com>
2019-03-11 17:58:27 +0000

    Add thread0 for riscv.

---
+++ sys/riscv/riscv/machdep.c
@@ -0,0 +1,80 @@
+/*-
+ * Copyright (c) 2019 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <sys/systm.h>
+#include <sys/thread.h>
+
+#include <machine/machdep.h>
+#include <machine/frame.h>
+#include <machine/cpufunc.h>
+
+static struct thread thread0;
+
+void
+critical_enter(void)
+{
+	struct thread *td;
+
+	td = curthread;
+
+	if (td->td_critnest == 0) {
+		td->td_critnest = 1;
+		td->td_md.md_saved_sstatus_ie = intr_disable();
+	} else
+		td->td_critnest++;
+}
+
+void
+critical_exit(void)
+{
+	struct thread *td;
+
+	td = curthread;
+
+	td->td_critnest--;
+	if (td->td_critnest == 0)
+		intr_restore(td->td_md.md_saved_sstatus_ie);
+}
+
+void
+cpu_idle(int busy)
+{
+
+	critical_enter();
+	__asm __volatile(
+	    "fence \n"
+	    "wfi   \n");
+	critical_exit();
+}
+
+void
+md_init(void)
+{
+
+	thread0.td_name = "the only thread";
+	curthread = &thread0;
+}
