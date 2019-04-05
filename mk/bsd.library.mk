a370eaec3c063e17bc3a8d297b5b5c18d44df929
Ruslan Bukin <br@bsdpad.com>
2019-04-05 22:23:55 +0100

    Use postfix _OBJS instead of _OBJECTS

--- mk/bsd.library.mk
+++ mk/bsd.library.mk
@@ -1,9 +1,9 @@
 .for lib0 in ${LIBRARIES}
 lib=${lib0:tu}
-.if empty(${lib}_OBJECTS)
+.if empty(${lib}_OBJS)
 .error Error: library "${lib}" not found
 .endif
-.for obj in ${${lib}_OBJECTS}
+.for obj in ${${lib}_OBJS}
 CFLAGS_${OSDIR}/${obj}+=${${lib}_CFLAGS}
 CFLAGS_${OSDIR}/${obj}+=-I${.OBJDIR}/machine/
 OBJECTS+=${OSDIR}/${obj}
--- mk/gnu.library.mk
+++ mk/gnu.library.mk
@@ -1,9 +1,9 @@
 $(foreach lib0,${LIBRARIES},						\
 	${eval lib=$(shell echo ${lib0} | tr '[:lower:]' '[:upper:]')}	\
-	$(if $(filter %, ${${lib}_OBJECTS}),,				\
+	$(if $(filter %, ${${lib}_OBJS}),,				\
 		$(error Error: library "${lib}" not found)		\
 	endif)								\
-	$(foreach obj,${${lib}_OBJECTS},				\
+	$(foreach obj,${${lib}_OBJS},					\
 		${eval CFLAGS_${OSOBJDIR}/${obj} += ${${lib}_CFLAGS}}	\
 		${eval CFLAGS_${OSOBJDIR}/${obj} += -I${OBJDIR}/machine/}\
 		${eval OBJECTS+=${OSOBJDIR}/${obj}}			\
