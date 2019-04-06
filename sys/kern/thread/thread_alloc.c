347aa74495367ba2c343da09ba1421b467a4603d
Ruslan Bukin <br@bsdpad.com>
2019-04-06 22:30:54 +0100

    Add thread_alloc.c.

---
+++ sys/kern/thread/thread_alloc.c
@@ -0,0 +1,74 @@
+/*-
+ * Copyright (c) 2019 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <sys/systm.h>
+#include <sys/thread.h>
+#include <sys/malloc.h>
+
+#include <machine/frame.h>
+
+#define	THREAD_DEBUG
+#undef	THREAD_DEBUG
+
+#ifdef	THREAD_DEBUG
+#define	dprintf(fmt, ...)	printf(fmt, ##__VA_ARGS__)
+#else
+#define	dprintf(fmt, ...)
+#endif
+
+struct thread *
+thread_alloc(uint32_t stack_size)
+{
+	struct thread *td;
+
+	td = zalloc(sizeof(struct thread));
+	if (td == NULL)
+		return (NULL);
+	td->td_mem_size = stack_size;
+	td->td_mem = zalloc(td->td_mem_size);
+	if (td->td_mem == NULL) {
+		free(td);
+		return (NULL);
+	}
+
+	return (td);
+}
+
+struct thread *
+thread_create(const char *name, uint32_t quantum,
+    uint32_t stack_size, void *entry, void *arg)
+{
+	struct thread *td;
+
+	td = thread_alloc(stack_size);
+	if (td == NULL)
+		return (NULL);
+
+	thread_setup(td, name, quantum, entry, arg);
+
+	return (td);
+}
