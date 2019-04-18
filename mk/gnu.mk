b59f380b4195684ee6814f5a17e88938d1a870f2
Ruslan Bukin <br@bsdpad.com>
2019-04-18 22:15:50 +0100

    Try to load all the included libraries.

--- mk/gnu.mk
+++ mk/gnu.mk
@@ -2,6 +2,16 @@ OSDIR ?= ${CURDIR}/osfive
 OBJDIR ?= ${CURDIR}/obj
 OSOBJDIR = ${OBJDIR}/${OSDIR}
 
+include ${OSDIR}/lib/libaeabi/Makefile.inc
+include ${OSDIR}/lib/libc/Makefile.inc
+include ${OSDIR}/lib/libfdt/Makefile.inc
+include ${OSDIR}/lib/libfont/Makefile.inc
+include ${OSDIR}/lib/libFLAC/Makefile.inc
+include ${OSDIR}/lib/mbedtls/Makefile.inc
+include ${OSDIR}/lib/md5/Makefile.inc
+include ${OSDIR}/lib/tinycrypt/Makefile.inc
+include ${OSDIR}/lib/xz-embedded/Makefile.inc
+
 include ${OSDIR}/mk/gnu.binutils.mk
 include ${OSDIR}/mk/gnu.clean.mk
 include ${OSDIR}/mk/gnu.compile.mk
