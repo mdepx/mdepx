72a0b9222cbe090c2d593620ef7e805e9ac877ad
Ruslan Bukin <br@bsdpad.com>
2019-04-18 20:14:40 +0100

    Fix style.

--- mk/gnu.user.mk
+++ mk/gnu.user.mk
@@ -2,6 +2,6 @@ ifndef APP
 $(error Error: APP must be defined)
 endif
 
-ifneq ("$(wildcard ${HOME}/.osfive/${APP}.mk)","")
+ifneq ("$(wildcard ${HOME}/.osfive/${APP}.mk)", "")
 include ${HOME}/.osfive/${APP}.mk
 endif
