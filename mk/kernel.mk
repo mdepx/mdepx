8fc49eecf8ef29708225223eaff0cd6acf025513
Ruslan Bukin <br@bsdpad.com>
2019-04-22 22:12:24 +0100

    Sort library and kernel objects.

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -15,6 +15,6 @@ endif
 KOBJECTS = $(shell ${PYTHON} ${OSDIR}/tools/files.py		\
 	${OSDIR} sys/conf/files.${MACHINE} ${KERNEL})
 
-$(foreach obj, ${KOBJECTS},					\
+$(foreach obj, $(sort ${KOBJECTS}),				\
 	${eval OBJECTS += ${OSOBJDIR}/${obj}}			\
 )
--- mk/library.mk
+++ mk/library.mk
@@ -3,12 +3,14 @@ $(foreach itm, ${LIBRARIES},						\
 	$(if $(filter %, ${${lib}_OBJS}),,				\
 		$(error Error: library "${lib}" not found)		\
 	endif)								\
-	$(foreach obj,${${lib}_OBJS},					\
+	$(foreach obj, ${${lib}_OBJS},					\
 		${eval CFLAGS_${OSOBJDIR}/${obj} += ${${lib}_CFLAGS}}	\
-		${eval OBJECTS+=${OSOBJDIR}/${obj}}			\
-		$(foreach inc,${${lib}_INCS},				\
+		${eval LOBJECTS += ${OSOBJDIR}/${obj}}			\
+		$(foreach inc, ${${lib}_INCS},				\
 			${eval CFLAGS_${OSOBJDIR}/${obj} +=		\
 			    -I${OSDIR}/${inc}}				\
 		)							\
 	)								\
 )
+
+OBJECTS += $(sort ${LOBJECTS})
