74dae8935b9029b411a7649419be7a496b9799f3
Ruslan Bukin <br@bsdpad.com>
2019-04-29 09:31:13 +0100

    Remove debugging printf.

--- sys/riscv/riscv/machdep.c
+++ sys/riscv/riscv/machdep.c
@@ -74,8 +74,6 @@ md_setup_frame(struct trapframe *tf, void *entry,
     void *arg, void *terminate)
 {
 
-	printf("%s\n", __func__);
-
 	tf->tf_mepc = (register_t)entry;
 	tf->tf_a[0] = (register_t)arg;
 	tf->tf_ra = (register_t)terminate;
