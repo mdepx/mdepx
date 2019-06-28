323e0f16861b32001ec29d990b34c3cf70d60c64
Ruslan Bukin <br@bsdpad.com>
2019-06-28 13:54:09 +0100

    Add extended descriptor format.

--- sys/dev/altera/msgdma/msgdma.h
+++ sys/dev/altera/msgdma/msgdma.h
@@ -65,15 +65,36 @@
 #define	PF_STATUS			0x10
 #define	 PF_STATUS_IRQ			(1 << 0)
 
-/* Prefetcher-disabled descriptor format. */
-struct msgdma_desc_nonpf {
-	uint32_t src_addr;
-	uint32_t dst_addr;
+#if defined(ALTERA_MSGDMA_DESC_STD)
+
+/* Standard descriptor format with prefetcher disabled. */
+struct msgdma_desc {
+	uint32_t read_lo;
+	uint32_t write_lo;
 	uint32_t length;
 	uint32_t control;
 };
 
-/* Prefetcher-enabled descriptor format. */
+#elif defined(ALTERA_MSGDMA_DESC_EXT)
+
+/* Extended descriptor format with prefetcher disabled. */
+struct msgdma_desc {
+	uint32_t read_lo;
+	uint32_t write_lo;
+	uint32_t length;
+	uint8_t write_burst;
+	uint8_t read_burst;
+	uint16_t seq_num;
+	uint16_t write_stride;
+	uint16_t read_stride;
+	uint32_t read_hi;
+	uint32_t write_hi;
+	uint32_t control;
+};
+
+#elif defined(ALTERA_MSGDMA_DESC_PF_STD)
+
+/* Standard descriptor format with prefetcher enabled. */
 struct msgdma_desc {
 	uint32_t read_lo;
 	uint32_t write_lo;
@@ -84,3 +105,34 @@ struct msgdma_desc {
 	uint32_t reserved;
 	uint32_t control;
 };
+
+#elif defined(ALTERA_MSGDMA_DESC_PF_EXT)
+
+/* Extended descriptor format with prefetcher enabled. */
+struct msgdma_desc {
+	uint32_t read_lo;
+	uint32_t write_lo;
+	uint32_t length;
+	uint32_t next;
+	uint32_t transferred;
+	uint32_t status;
+	uint32_t reserved;
+	uint8_t write_burst;
+	uint8_t read_burst;
+	uint16_t seq_num;
+	uint16_t write_stride;
+	uint16_t read_stride;
+	uint32_t read_hi;
+	uint32_t write_hi;
+	uint32_t next_hi;
+	uint32_t reserved1;
+	uint32_t reserved2;
+	uint32_t reserved3;
+	uint32_t control;
+};
+
+#else
+
+#error	"mSGDMA descriptor format (kernel option) is not set."
+
+#endif
