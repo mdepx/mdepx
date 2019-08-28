35f8782ecc035237e868d8dffdc84cfae3dcbedd
Ruslan Bukin <br@bsdpad.com>
2019-08-28 22:34:34 +0100

    Remove old libraries interface

--- mk/library.mk
+++ mk/library.mk
@@ -1,21 +1,5 @@
 OSOBJDIR = ${OBJDIR}/${OSDIR}
 
-$(foreach itm, ${LIBRARIES},						\
-	${eval lib = $(shell echo ${itm} | tr '[:lower:]' '[:upper:]')}	\
-	$(if $(filter %, ${${lib}_OBJS}),,				\
-		$(error Error: library "${lib}" not found)		\
-	endif)								\
-	$(foreach obj, ${${lib}_OBJS},					\
-		${eval CFLAGS_${OSOBJDIR}/${obj} += ${${lib}_CFLAGS}}	\
-		${eval OBJECTS += ${OSDIR}/${obj}}			\
-		$(foreach inc, ${${lib}_INCS},				\
-			${eval CFLAGS_${OSOBJDIR}/${obj} +=		\
-			    -I${OSDIR}/${inc}}				\
-		)							\
-	)								\
-)
-
-# New interface
 LCMD = ${PYTHON} -B ${OSDIR}/tools/emitter.py
 LIBS = $(shell ${LCMD} ${MDX_CONFIG} ${OBJDIR} ${OSDIR})
 $(foreach lib, ${LIBS},			\
