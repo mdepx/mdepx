14ec9a63120da567ebfeea444ec463af73b3a886
Ruslan Bukin <br@bsdpad.com>
2019-08-29 12:35:43 +0100

    Rename config files

--- mk/emitter.mk
+++ mk/emitter.mk
@@ -6,8 +6,8 @@ endif
 
 OSOBJDIR = ${OBJDIR}/${OSDIR}
 
-LCMD = ${PYTHON} -B ${OSDIR}/tools/emitter.py
-LIBS = $(shell ${LCMD} ${MDX_CONFIG} ${OBJDIR} ${OSDIR})
-$(foreach lib, ${LIBS},			\
-	${eval ${lib}}			\
+ECMD = ${PYTHON} -B ${OSDIR}/tools/emitter.py
+EOUTPUT = $(shell ${ECMD} ${MDX_CONFIG} ${OBJDIR} ${OSDIR})
+$(foreach e, ${EOUTPUT},		\
+	${eval ${e}}			\
 );
