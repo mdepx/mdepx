3f12797387bbbdf5856ad2cab1a3a961e91c228d
Ruslan Bukin <br@bsdpad.com>
2019-09-05 18:13:34 +0100

    Support for recursively modules loading.

--- tools/emitter.py
+++ tools/emitter.py
@@ -1,36 +1,37 @@
-import sys
-import os
-from parser import proc0
 from flags import collect_all_user_flags
 from flags import collect_flags, print_flags
+from parser import proc0
+import sys
+import os
 
-c_dirs = {'library': 'lib', 'kernel': 'kernel'}
-
-def obj_set_flags(resobj, context, key, l):
+def obj_set_flags(resobj, context, root, key, l):
 	if not 'objects' in context:
 		return
 	for obj in context['objects']:
-		if not obj in resobj:
-			resobj[obj] = {}
-		if not key in resobj[obj]:
-			resobj[obj][key] = []
+		o = os.path.join(root, obj)
+		if not o in resobj:
+			resobj[o] = {}
+		if not key in resobj[o]:
+			resobj[o][key] = []
 		for el in l:
-			resobj[obj][key].append(el)
+			resobj[o][key].append(el)
 
-def proc1(resobj, flags, config, context_str):
-	if not context_str in config:
-		return resobj
-	context = config[context_str]
+def proc1(resobj, flags, root, context, context_str):
+	obj_set_flags(resobj, context, root, 'incs', [])
+
 	if not 'module' in context:
 		return
 	for m in context['module']:
 		options = []
 		if m in context:
 			node = context[m]
+			proc1(resobj, flags, context_str, node, m)
 			if 'options' in node:
 				options += node['options']
 
-		p = os.path.join(osdir, c_dirs[context_str], m, "mdepx.conf")
+		p = os.path.join(root, context_str, m, "mdepx.conf")
+		if not os.path.exists(p):
+			continue
 		f = open(p, "r")
 		data = f.read()
 		f.close()
@@ -51,8 +52,8 @@ def proc1(resobj, flags, config, context_str):
 		if 'cflags' in context1:
 			cflags += context1['cflags']
 
-		obj_set_flags(resobj, context1, 'incs', incs)
-		obj_set_flags(resobj, context1, 'cflags', cflags)
+		obj_set_flags(resobj, context1, root, 'incs', incs)
+		obj_set_flags(resobj, context1, root, 'cflags', cflags)
 
 		for opt in options:
 			incs1 = []
@@ -68,35 +69,33 @@ def proc1(resobj, flags, config, context_str):
 			if 'cflags' in node:
 				cflags1 += node['cflags']
 
-			obj_set_flags(resobj, node, 'incs', incs1 + incs)
-			obj_set_flags(resobj, node, 'cflags', cflags1 + cflags)
+			obj_set_flags(resobj, node, root, 'incs', incs1 + incs)
+			obj_set_flags(resobj, node, root, 'cflags', cflags1 + cflags)
 
 	return resobj
 
 def emit_objects_flags(resobj):
 	for obj in resobj:
-		print("OBJECTS+=%s/%s" % (osdir, obj))
+		print("OBJECTS+=%s" % obj)
 
 	for obj in resobj:
 		if 'incs' in resobj[obj]:
 			for inc in resobj[obj]['incs']:
 				print("CFLAGS_%s/%s+=-I${OSDIR}/%s" % \
-					(osobjdir, obj, inc))
+					(objdir, obj, inc))
 
 		if 'cflags' in resobj[obj]:
 			for cflag in resobj[obj]['cflags']:
 				print("CFLAGS_%s/%s+=%s" % \
-					(osobjdir, obj, cflag))
+					(objdir, obj, cflag))
 
 if __name__ == '__main__':
 	args = sys.argv
-	if len(args) < 4:
+	if len(args) < 3:
 		sys.exit(1)
 
 	config_file = args[1]
 	objdir = args[2]
-	osdir = args[3]
-	osobjdir = "%s/%s" % (objdir, osdir)
 
 	if not os.path.exists(config_file):
 		sys.exit(2)
@@ -108,12 +107,13 @@ if __name__ == '__main__':
 	proc0(config, config_str)
 
 	flags = {}
-
 	resobj = {}
-	proc1(resobj, flags, config, 'library')
-	proc1(resobj, flags, config, 'kernel')
+	proc1(resobj, flags, '', config, '')
 	emit_objects_flags(resobj)
 
-	collect_all_user_flags(flags, config['kernel'])
+	if 'mdepx' in config:
+		if 'kernel' in config['mdepx']:
+			kernel = config['mdepx']['kernel']
+			collect_all_user_flags(flags, kernel)
 
 	print_flags(flags)
