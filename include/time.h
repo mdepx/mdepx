8ed69b29b58c617bca639c925a60d5f88959e653
Ruslan Bukin <br@bsdpad.com>
2020-01-09 11:55:12 +0000

    Add time.h.

---
+++ include/time.h
@@ -0,0 +1,44 @@
+/*-
+ * Copyright (c) 2020 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_TIME_H_
+#define	_TIME_H_
+
+#include <sys/types.h>
+
+struct tm {
+	int	tm_sec;		/* Seconds [0-60] (could be leap). */
+	int	tm_min;		/* Minutes [0-59]. */
+	int	tm_hour;	/* Hours [0-23]. */
+	int	tm_mday;	/* Day of the month [0-31]. */
+	int	tm_mon;		/* Month [0-11]. */
+	int	tm_year;	/* Years since 1900. */
+	int	tm_wday;	/* Day of the week since Sun [0-6]. */
+	int	tm_yday;	/* Days since January 1 [0-365]. */
+	int	tm_isdst;	/* Daylight saving time flag. */
+};
+
+#endif /* !_TIME_H_ */
