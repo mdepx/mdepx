28043b0426245e4cc348375d8aa108560d0c6640
Ruslan Bukin <br@bsdpad.com>
2020-04-04 00:26:22 +0100

    Add strspn().

--- lib/libc/string/strspn.c
+++ lib/libc/string/strspn.c
@@ -1,5 +1,7 @@
 /*-
- * Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2005 David Schultz <das@FreeBSD.ORG>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,30 +26,47 @@
  * SUCH DAMAGE.
  */
 
-#ifndef _STRING_H_
-#define _STRING_H_
+#include <sys/cdefs.h>
 
+#include <sys/libkern.h>
+#include <sys/limits.h>
 #include <sys/types.h>
 
-size_t strlen(const char *s);
-size_t strnlen(const char *s, size_t maxlen);
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t len);
-void * memcpy(void *dst, const void *src, size_t len);
-void * memset(void *dest, int c, size_t len);
-void * memmove(void *dst, const void *src, size_t len);
-int memcmp(const void *b1, const void *b2, size_t len);
-void * memchr(const void *b, int c, size_t len);
-char * strsep(char **stringp, const char *delim);
-char * strcat(char * restrict s, const char * restrict append);
-char * strncat(char * restrict s, const char * restrict append, size_t count);
-char * strstr(const char *big, const char *little);
-char * strchr(const char *s, int c);
-char * strcpy(char * restrict dst, const char * restrict src);
-size_t strcspn(const char *s, const char *charset);
-size_t strlcpy(char * restrict dst, const char * restrict src, size_t dstsize);
-size_t strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
-char * strrchr(const char *s, int c);
-size_t strspn(const char *s, const char *accept);
+#define	IDX(c)	((u_char)(c) / LONG_BIT)
+#define	BIT(c)	((u_long)1 << ((u_char)(c) % LONG_BIT))
 
-#endif /* !_STRING_H_ */
+size_t
+strspn(const char *s, const char *charset)
+{
+	/*
+	 * NB: idx and bit are temporaries whose use causes gcc 3.4.2 to
+	 * generate better code.  Without them, gcc gets a little confused.
+	 */
+	const char *s1;
+	u_long bit;
+	u_long tbl[(UCHAR_MAX + 1) / LONG_BIT];
+	int idx;
+
+	if(*s == '\0')
+		return (0);
+
+#if LONG_BIT == 64	/* always better to unroll on 64-bit architectures */
+	tbl[3] = tbl[2] = tbl[1] = tbl[0] = 0;
+#else
+	for (idx = 0; idx < sizeof(tbl) / sizeof(tbl[0]); idx++)
+		tbl[idx] = 0;
+#endif
+	for (; *charset != '\0'; charset++) {
+		idx = IDX(*charset);
+		bit = BIT(*charset);
+		tbl[idx] |= bit;
+	}
+
+	for(s1 = s; ; s1++) {
+		idx = IDX(*s1);
+		bit = BIT(*s1);
+		if ((tbl[idx] & bit) == 0)
+			break;
+	}
+	return (s1 - s);
+}
