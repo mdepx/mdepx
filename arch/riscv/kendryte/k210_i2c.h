f6a70f7d1c27659caa12c377207a40d99990f0e0
Ruslan Bukin <br@bsdpad.com>
2020-04-24 10:03:15 +0100

    Add i2c registers.

---
+++ arch/riscv/kendryte/k210_i2c.h
@@ -0,0 +1,77 @@
+/*-
+ * Copyright (c) 2020 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _RISCV_KENDRYTE_K210_I2C_H_
+#define _RISCV_KENDRYTE_K210_I2C_H_
+
+#define	K210_I2C_CON		0x00	/* Control */
+#define	K210_I2C_TAR		0x04	/* Target Address */
+#define	K210_I2C_SAR		0x08	/* Slave Address */
+#define	K210_I2C_RESV1		0x0c	/* reserved */
+#define	K210_I2C_DATA_CMD	0x10	/* Data Buffer and Command */
+#define	K210_I2C_SS_SCL_HCNT	0x14	/* Std Speed Clock SCL High Count */
+#define	K210_I2C_SS_SCL_LCNT	0x18	/* Std Speed Clock SCL Low Count */
+#define	K210_I2C_INTR_STAT	0x2c	/* Interrupt Status */
+#define	K210_I2C_INTR_MASK	0x30	/* Interrupt Mask */
+#define	K210_I2C_RAW_INTR_STAT	0x34	/* Raw Interrupt Status */
+#define	K210_I2C_RX_TL		0x38	/* Receive FIFO Threshold */
+#define	K210_I2C_TX_TL		0x3c	/* Transmit FIFO Threshold */
+#define	K210_I2C_CLR_INTR	0x40	/* Clear Combined and Individual Int.*/
+#define	K210_I2C_CLR_RX_UNDER	0x44	/* Clear RX_UNDER Interrupt */
+#define	K210_I2C_CLR_RX_OVER	0x48	/* Clear RX_OVER Interrupt */
+#define	K210_I2C_CLR_TX_OVER	0x4c	/* Clear TX_OVER Interrupt */
+#define	K210_I2C_CLR_RD_REQ	0x50	/* Clear RD_REQ Interrupt */
+#define	K210_I2C_CLR_TX_ABRT	0x54	/* Clear TX_ABRT Interrupt */
+#define	K210_I2C_CLR_RX_DONE	0x58	/* Clear RX_DONE Interrupt */
+#define	K210_I2C_CLR_ACTIVITY	0x5c	/* Clear ACTIVITY Interrupt */
+#define	K210_I2C_CLR_STOP_DET	0x60	/* Clear STOP_DET Interrupt */
+#define	K210_I2C_CLR_START_DET	0x64	/* Clear START_DET Interrupt */
+#define	K210_I2C_CLR_GEN_CALL	0x68	/* Clear GEN_CALL Interrupt */
+#define	K210_I2C_ENABLE		0x6c	/* Enable */
+#define	K210_I2C_STATUS		0x70	/* Status */
+#define	K210_I2C_TXFLR		0x74	/* Transmit FIFO Level */
+#define	K210_I2C_RXFLR		0x78	/* Receive FIFO Level */
+#define	K210_I2C_SDA_HOLD	0x7c	/* SDA Hold Time Length */
+#define	K210_I2C_TX_ABRT_SOURCE	0x80	/* Transmit Abort Source */
+#define	K210_I2C_RESV3		0x84	/* reserved */
+#define	K210_I2C_DMA_CR		0x88	/* DMA Control */
+#define	K210_I2C_DMA_TDLR	0x8c	/* DMA Transmit Data Level */
+#define	K210_I2C_DMA_RDLR	0x90	/* DMA Receive Data Level */
+#define	K210_I2C_SDA_SETUP	0x94	/* SDA Setup */
+#define	K210_I2C_GENERAL_CALL	0x98	/* ACK General Call */
+#define	K210_I2C_ENABLE_STATUS	0x9c	/* Enable Status */
+#define	K210_I2C_FS_SPKLEN	0xa0	/* SS,FS or FM+ spike suppress limit */
+#define	K210_I2C_COMP_PARAM_1	0xf4	/* Component Parameter 1 */
+#define	K210_I2C_COMP_VERSION	0xf8	/* Component Version */
+#define	K210_I2C_COMP_TYPE	0xfc	/* Component Type */
+
+struct k210_i2c_softc {
+	size_t base;
+};
+
+void k210_i2c_init(struct k210_i2c_softc *sc, uint32_t base);
+
+#endif /* !_RISCV_KENDRYTE_K210_I2C_H_ */
