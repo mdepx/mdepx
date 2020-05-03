e6c511ff21436711e2e3ed36a6a762de5e8e8df5
Ruslan Bukin <br@bsdpad.com>
2020-05-03 18:13:59 +0100

    Add some macroses and FIFO drain function.

--- app/co2/co2.c
+++ app/co2/co2.c
@@ -44,26 +44,28 @@ static struct i2c_bitbang_softc i2c_bitbang_sc;
 extern struct k210_i2c_softc i2c_sc;
 extern struct uart_16550_softc uart_sc;
 
+#define	PIN_BME680_SDO	26
+#define	PIN_I2C_SCL	28
+#define	PIN_I2C_SDA	29
+
 static void
 i2c_scl(struct i2c_bitbang_softc *sc, bool enable)
 {
 
-	if (enable) {
-		mdx_gpio_configure(0, 28, MDX_GPIO_INPUT);
-	} else {
-		mdx_gpio_configure(0, 28, MDX_GPIO_OUTPUT);
-	}
+	if (enable)
+		mdx_gpio_configure(0, PIN_I2C_SCL, MDX_GPIO_INPUT);
+	else
+		mdx_gpio_configure(0, PIN_I2C_SCL, MDX_GPIO_OUTPUT);
 }
 
 static void
 i2c_sda(struct i2c_bitbang_softc *sc, bool enable)
 {
 
-	if (enable) {
-		mdx_gpio_set(0, 29, 0);
-	} else {
-		mdx_gpio_set(0, 29, 1);
-	}
+	if (enable)
+		mdx_gpio_set(0, PIN_I2C_SDA, 0);
+	else
+		mdx_gpio_set(0, PIN_I2C_SDA, 1);
 }
 
 static int
@@ -71,7 +73,7 @@ i2c_sda_val(struct i2c_bitbang_softc *sc)
 {
 	uint8_t reg;
 
-	reg = mdx_gpio_get(0, 29);
+	reg = mdx_gpio_get(0, PIN_I2C_SDA);
 
 	return (reg & 0x1);
 }
@@ -93,8 +95,8 @@ bme680_sensor_init(void)
 	i2cb.xfer = k210_i2c_xfer;
 	i2cb.arg = &i2c_sc;
 
-	mdx_gpio_set(0, 28, 0);
-	mdx_gpio_set(0, 29, 0);
+	mdx_gpio_set(0, PIN_I2C_SCL, 0);
+	mdx_gpio_set(0, PIN_I2C_SDA, 0);
 
 	dev.i2cb = &i2cb;
 	ret = bme680_initialize(&dev);
@@ -103,14 +105,28 @@ bme680_sensor_init(void)
 }
 
 static void
-mh_z19_cycle(uint8_t *req, uint8_t *reply)
+mh_z19b_cycle(uint8_t *req, uint8_t *reply, int reply_len)
 {
 	int i;
 
 	for (i = 0; i < 9; i++)
 		uart_16550_putc(&uart_sc, req[i]);
-	for (i = 0; i < 9; i++)
+
+	for (i = 0; i < reply_len; i++)
 		reply[i] = uart_16550_getc(&uart_sc);
+}
+
+static void
+drain_fifo(void)
+{
+	bool ready;
+
+	for (;;) {
+		ready = uart_16550_rxready(&uart_sc);
+		if (!ready)
+			break;
+		uart_16550_getc(&uart_sc);
+	}
 }
 
 int
@@ -124,30 +140,38 @@ main(void)
 
 	printf("Hello world!\n");
 
-	mdx_gpio_set(0, 20, 1);
-	mdx_gpio_set(0, 21, 1);
-	mdx_gpio_set(0, 22, 1);
+	/*
+	 * bme680 SDO
+	 * 1: sets i2c address to 0x77
+	 * 0: sets i2c address to 0x76
+	 */
+	mdx_gpio_configure(0, PIN_BME680_SDO, MDX_GPIO_OUTPUT);
+	mdx_gpio_set(0, PIN_BME680_SDO, 1);
 
-	/* bme680 SDO CS */
-	mdx_gpio_configure(0, 26, MDX_GPIO_OUTPUT);
-	mdx_gpio_configure(0, 27, MDX_GPIO_OUTPUT);
-	mdx_gpio_set(0, 26, 1);
-	mdx_gpio_set(0, 27, 1);
-
-	mdx_gpio_set(0, 20, 0);
-	mdx_gpio_set(0, 22, 1);
+	/*
+	 * Note that for I2C interface bme680 CS pin must be set to HIGH
+	 * at same time as power supplied to bme680, otherwise it will
+	 * be locked to SPI protocol.
+	 */
 
 	error = bme680_sensor_init();
 	if (error)
 		printf("could not initialize bme680\n");
 
+	drain_fifo();
+
 	mh_z19b_set_range_req(req, 2000);
-	mh_z19_cycle(req, reply);
+	mh_z19b_cycle(req, reply, 2);
+
+	/* Measurement range changed, wait a bit. */
+	mdx_usleep(5000000);
+
+	drain_fifo();
 
 	mh_z19b_read_co2_req(req);
 
 	while (1) {
-		mh_z19_cycle(req, reply);
+		mh_z19b_cycle(req, reply, 9);
 
 		error = mh_z19b_read_co2_reply(reply, &co2);
 		if (error) {
@@ -159,12 +183,12 @@ main(void)
 		error = bme680_read_data(&dev, &data);
 		if (error == 0) {
 			printf("CO2 %d ppm, temp %d\n", co2, data.temperature);
-			mdx_usleep(2000000);
-			bme680_trigger(&dev);
-			mdx_usleep(2000000);
 		} else
 			printf("Failed to read bme680 data, error %d\n",
 			    error);
+
+		bme680_trigger(&dev);
+		mdx_usleep(5000000);
 	}
 
 	return (0);
