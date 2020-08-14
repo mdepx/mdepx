a528159334ddd33059c63612bceea35cab1ea607
Ruslan Bukin <br@bsdpad.com>
2020-08-14 13:50:16 +0100

    Add support for exceptions in purecap mode.

--- demo/qemu-cheri128/pure/main.c
+++ demo/qemu-cheri128/pure/main.c
@@ -29,11 +29,15 @@
 #include <sys/endian.h>
 #include <sys/systm.h>
 #include <sys/malloc.h>
+#include <sys/thread.h>
 
 #include <machine/frame.h>
 #include <machine/cpuregs.h>
 #include <machine/cpufunc.h>
 #include <machine/cheric.h>
+
+#include <mips/mips/trap.h>
+#include <mips/mips/timer.h>
 
 #include <dev/uart/uart_16550.h>
 
@@ -44,15 +48,82 @@ extern char MipsCache[], MipsCacheEnd[];
 #define	UART_BASE		0x180003f8
 #define	UART_CLOCK_RATE		3686400
 #define	DEFAULT_BAUDRATE	115200
-#define	MIPS_DEFAULT_FREQ	1000000
+#define	MIPS_DEFAULT_FREQ	50000000 /* 50MHz clock */
 
+static struct mips_timer_softc timer_sc;
 static struct uart_16550_softc uart_sc;
+
 struct mdx_device uart = {.sc = &uart_sc };
 
 void * __capability kernel_sealcap;
 
 void cpu_reset(void);
 int main(void);
+
+static void
+softintr(void *arg, struct trapframe *frame, int i)
+{
+	uint32_t cause;
+
+	printf("Soft interrupt %d\n", i);
+
+	cause = mips_rd_cause();
+	cause &= ~(1 << (8 + i));
+	mips_wr_cause(cause);
+};
+
+static void
+hardintr_unknown(void *arg, struct trapframe *frame, int i)
+{
+
+	printf("Unknown hard interrupt %d\n", i);
+}
+
+static void
+hardintr(void *arg, struct trapframe *frame, int i)
+{
+}
+
+static void
+mips_install_vectors(void)
+{
+	capability cap;
+	capability a, b;
+
+#if 0
+	if (MipsCacheEnd - MipsCache > 0x80)
+		panic("Cache error code is too big");
+	if (MipsTLBMissEnd - MipsTLBMiss > 0x80)
+		panic("TLB code is too big");
+#endif
+
+	a = MipsException;
+
+	/*
+	 * Mips exception code has two instructions only,
+	 * so copy 8 bytes.
+	 */
+	cap = cheri_getdefault();
+	b = cheri_setoffset(cap, MIPS_EXC_VEC_GENERAL);
+	b = cheri_csetbounds(b, 8);
+	bcopy(a, b, 8);
+
+	b = cheri_setoffset(cap, CHERI_CCALL_EXC_VEC);
+	b = cheri_csetbounds(b, 8);
+	bcopy(a, b, 8);
+
+#if 0
+	/* Install Cache Error code */
+	bcopy(MipsCache, (void *)MIPS_EXC_VEC_CACHE_ERR,
+	    MipsCacheEnd - MipsCache);
+
+	/* Install TLB exception code */
+	bcopy(MipsTLBMiss, (void *)MIPS_EXC_VEC_UTLB_MISS,
+	    MipsTLBMissEnd - MipsTLBMiss);
+	bcopy(MipsTLBMiss, (void *)MIPS_EXC_VEC_XTLB_MISS,
+	    MipsTLBMissEnd - MipsTLBMiss);
+#endif
+}
 
 void
 udelay(uint32_t usec)
@@ -80,10 +151,6 @@ setup_uart(void)
 	capability cap;
 
 	cap = cheri_getdefault();
-
-	/* Remove default capability */
-	__asm __volatile("csetdefault $cnull");
-
 	cap = cheri_setoffset(cap, MIPS_XKPHYS_UNCACHED_BASE + UART_BASE);
 	cap = cheri_csetbounds(cap, 6);
 
@@ -95,15 +162,52 @@ setup_uart(void)
 	mdx_console_register_uart(&uart);
 }
 
-int
-main(void)
+void
+board_init(void)
 {
+	int status;
+
+	mips_install_vectors();
+
+	mips_setup_intr(0, softintr, NULL);
+	mips_setup_intr(1, softintr, NULL);
+	mips_setup_intr(2, hardintr, NULL);
+	mips_setup_intr(3, hardintr_unknown, NULL);
+	mips_setup_intr(4, hardintr_unknown, NULL);
+	mips_setup_intr(5, hardintr_unknown, NULL);
+	mips_setup_intr(6, hardintr_unknown, NULL);
+	mips_setup_intr(7, mips_timer_intr, &timer_sc);
+
+	status = mips_rd_status();
+	status &= ~(MIPS_SR_IM_M);
+	status |= MIPS_SR_IM_HARD(5);
+	status |= MIPS_SR_IE;
+	status &= ~MIPS_SR_BEV;
+	status |= MIPS_SR_UX;
+	status |= MIPS_SR_KX;
+	status |= MIPS_SR_SX;
+	mips_wr_status(status);
 
 	/* Setup capability-enabled JTAG UART. */
 	setup_uart();
 
-	while (1)
+	/* Remove default capability */
+	__asm __volatile("csetdefault $cnull");
+}
+
+int
+main(void)
+{
+	int error;
+
+	error = mips_timer_init(&timer_sc, MIPS_DEFAULT_FREQ);
+	if (error)
+		printf("can't register the timer\n");
+
+	while (1) {
 		printf("Hello Pure Capability World\n");
+		mdx_usleep(1000000);
+	}
 
 	return (0);
 }
