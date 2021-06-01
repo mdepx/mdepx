019aa7adba5fe68320547a5119234918f33172ee
Ruslan Bukin <br@bsdpad.com>
2021-06-01 16:34:40 +0100

    Add system ticker. This is optional module needed to count system uptime.

---
+++ kernel/tick/mdx_tick.c
@@ -0,0 +1,79 @@
+/*-
+ * Copyright (c) 2021 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+#include <sys/callout.h>
+#include <sys/pcpu.h>
+#include <sys/systm.h>
+#include <sys/thread.h>
+#include <sys/tick.h>
+
+#include <machine/atomic.h>
+
+static struct mdx_callout c;
+static uint64_t mdx_uptime_ms;
+static uint32_t started = 0;
+
+static void
+mdx_tick(void *arg)
+{
+
+	KASSERT(curthread->td_critnest > 0,
+	    ("%s: Not in critical section.", __func__));
+
+	mdx_uptime_ms += MDX_TICK_PERIOD_US / 1000;
+	mdx_callout_set_locked(&c, MDX_TICK_PERIOD_US, mdx_tick, NULL);
+}
+
+/*
+ * Sleep given amount of ticks in multi-threaded environment.
+ */
+int
+mdx_tick_start(void)
+{
+	int res;
+
+	res = atomic_cmpset_32(&started, 0, 1);
+	if (!res)
+		return (MDX_ERROR);
+
+	mdx_callout_init(&c);
+	mdx_uptime_ms = 0;
+
+	mdx_callout_set(&c, MDX_TICK_PERIOD_US, mdx_tick, NULL);
+
+	return (0);
+}
+
+/*
+ * Returns time passed since ticker started.
+ */
+uint64_t
+mdx_uptime(void)
+{
+
+	return (mdx_uptime_ms);
+}
