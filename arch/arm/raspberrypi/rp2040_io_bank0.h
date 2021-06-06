e0651189670eb2606b8aeb0a0f3f6c98d1390584
Ruslan Bukin <br@bsdpad.com>
2021-06-06 19:33:54 +0100

    Add rp2040 io bank registers.

---
+++ arch/arm/raspberrypi/rp2040_io_bank0.h
@@ -0,0 +1,43 @@
+/*-
+ * Copyright (c) 2021 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef	_RP2040_IO_BANK0_H_
+#define	_RP2040_IO_BANK0_H_
+
+#define	RP2040_IO_BANK0_GPIO_STATUS(n)	(0x000 + 0x8 * (n))	/* n: 0..29 */
+#define	RP2040_IO_BANK0_GPIO_CTRL(n)	(0x004 + 0x8 * (n))
+#define	RP2040_IO_BANK0_INTR(n)		(0x0f0 + 0x4 * (n))	/* n: 0..3 */
+#define	RP2040_IO_BANK0_PROC0_INTE(n)	(0x100 * 0x4 * (n))
+#define	RP2040_IO_BANK0_PROC0_INTF(n)	(0x110 * 0x4 * (n))
+#define	RP2040_IO_BANK0_PROC0_INTS(n)	(0x120 * 0x4 * (n))
+#define	RP2040_IO_BANK0_PROC1_INTE(n)	(0x130 * 0x4 * (n))
+#define	RP2040_IO_BANK0_PROC1_INTF(n)	(0x140 * 0x4 * (n))
+#define	RP2040_IO_BANK0_PROC1_INTS(n)	(0x150 * 0x4 * (n))
+#define	RP2040_IO_BANK0_DORMANT_WAKE_INTE(n)	(0x160 * 0x4 * (n))
+#define	RP2040_IO_BANK0_DORMANT_WAKE_INTF(n)	(0x170 * 0x4 * (n))
+#define	RP2040_IO_BANK0_DORMANT_WAKE_INTS(n)	(0x180 * 0x4 * (n))
+
+#endif /* !_RP2040_IO_BANK0_H_ */
