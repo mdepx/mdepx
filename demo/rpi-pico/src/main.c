725e4ee45866991d410da3018b4ec5e92adc0287
Ruslan Bukin <br@bsdpad.com>
2021-07-10 14:56:08 +0100

    Add some test code.

--- demo/rpi-pico/src/main.c
+++ demo/rpi-pico/src/main.c
@@ -1,19 +1,44 @@
 #include <sys/cdefs.h>
 #include <sys/systm.h>
+#include <sys/thread.h>
+#include <dev/intc/intc.h>
+
+#include <arm/raspberrypi/rp2040.h>
 
 #include <app/callout_test/callout_test.h>
+
+static void __unused
+test_thr(void *arg)
+{
+	int n;
+
+	n = (int)arg;
+
+	while (1) {
+		printf("%s%d (%d)\n", __func__, n, PCPU_GET(cpuid));
+		mdx_usleep(100000 + 100000 * n);
+	}
+}
 
 int
 main(void)
 {
+	struct thread *td;
 
-#if 0
+#if 1
 	callout_test();
 #endif
 
+	td = mdx_thread_create("test", 1, 1000, 4096, test_thr, (void *)0);
+	mdx_sched_add(td);
+	td = mdx_thread_create("test1", 1, 1000, 4096, test_thr, (void *)1);
+	mdx_sched_add(td);
+	td = mdx_thread_create("test1", 1, 1000, 4096, test_thr, (void *)2);
+	mdx_sched_add(td);
+
 	while (1) {
-		printf("Hello world\n");
-		mdx_usleep(1000000);
+		printf("Hello world (%d)\n", PCPU_GET(cpuid));
+		mdx_usleep(1500000);
 	}
 
 	return (0);
