d829e6a2030e0c9a68b44630701a536a79c70801
Ruslan Bukin <br@bsdpad.com>
2021-09-20 19:05:44 +0100

    Check if types already declared.

--- include/sys/types.h
+++ include/sys/types.h
@@ -44,50 +44,65 @@ typedef	char *			__va_list;
 typedef	__va_list		__gnuc_va_list;	/* compatibility w/GNU headers*/
 #endif
 
-#ifndef _INT8_T_DECLARED
+#ifndef	_INT8_T_DECLARED
 typedef	__int8_t	int8_t;
 #define	_INT8_T_DECLARED
 #endif
 
-#ifndef _INT16_T_DECLARED
+#ifndef	_INT16_T_DECLARED
 typedef	__int16_t	int16_t;
 #define	_INT16_T_DECLARED
 #endif
 
-#ifndef _INT32_T_DECLARED
+#ifndef	_INT32_T_DECLARED
 typedef	__int32_t	int32_t;
 #define	_INT32_T_DECLARED
 #endif
 
-#ifndef _INT64_T_DECLARED
+#ifndef	_INT64_T_DECLARED
 typedef	__int64_t	int64_t;
 #define	_INT64_T_DECLARED
 #endif
 
-#ifndef _UINT8_T_DECLARED
+#ifndef	_UINT8_T_DECLARED
 typedef	__uint8_t	uint8_t;
 #define	_UINT8_T_DECLARED
 #endif
 
-#ifndef _UINT16_T_DECLARED
+#ifndef	_UINT16_T_DECLARED
 typedef	__uint16_t	uint16_t;
 #define	_UINT16_T_DECLARED
 #endif
 
-#ifndef _UINT32_T_DECLARED
+#ifndef	_UINT32_T_DECLARED
 typedef	__uint32_t	uint32_t;
 #define	_UINT32_T_DECLARED
 #endif
 
-#ifndef _UINT64_T_DECLARED
+#ifndef	_UINT64_T_DECLARED
 typedef	__uint64_t	uint64_t;
 #define	_UINT64_T_DECLARED
 #endif
 
-typedef	__uintptr_t	uintptr_t;
+#ifndef	_INTPTR_T_DECLARED
 typedef	__intptr_t	intptr_t;
-typedef	__uintmax_t	uintmax_t;
+#define	_INTPTR_T_DECLARED
+#endif
+
+#ifndef	_UINTPTR_T_DECLARED
+typedef	__uintptr_t	uintptr_t;
+#define	_UINTPTR_T_DECLARED
+#endif
+
+#ifndef	_INTMAX_T_DECLARED
 typedef	__intmax_t	intmax_t;
+#define	_INTMAX_T_DECLARED
+#endif
+
+#ifndef	_UINTMAX_T_DECLARED
+typedef	__uintmax_t	uintmax_t;
+#define	_UINTMAX_T_DECLARED
+#endif
 
 #ifndef	_SIZE_T_DECLARED
 typedef	__size_t	size_t;
