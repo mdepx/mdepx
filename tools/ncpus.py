b8270a2d09b7a5a0340b1a1824e785a46feb10f8
Ruslan Bukin <br@bsdpad.com>
2021-11-15 20:56:19 +0000

    Don't emulate more cpus than specified in mdepx config file.

--- tools/ncpus.py
+++ tools/ncpus.py
@@ -1,5 +1,16 @@
 import multiprocessing
+import argparse
 
-ncpus = multiprocessing.cpu_count()
+def print_ncpus():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-m", help="max cpus allowed", type = int)
+	args = parser.parse_args()
 
-print(ncpus)
+	ncpus = multiprocessing.cpu_count()
+	if (args.m and args.m < ncpus):
+		ncpus = args.m
+
+	print(ncpus)
+
+if __name__ == '__main__':
+	print_ncpus()
