1643c43da3f9f4abb621cb01e31ff7eed281e18a
Ruslan Bukin <br@freebsd.org>
2023-10-09 21:11:22 +0100

    Add Timer1 and NVIC base registers.

--- arch/arm/stm/stm32g0.h
+++ arch/arm/stm/stm32g0.h
@@ -28,6 +28,7 @@
 #define _ARM_STM_STM32G0_H_
 
 #include <arm/stm/stm32f4_gpio.h>
+#include <arm/stm/stm32f4_timer.h>
 #include <arm/stm/stm32g0_rcc.h>
 #include <arm/stm/stm32l4_usart.h>
 
@@ -36,5 +37,7 @@
 #define	RCC_BASE	0x40021000
 #define	FLASH_BASE	0x40022000
 #define	GPIO_BASE	0x50000000
+#define	TIM1_BASE	0x40012C00
+#define	NVIC_BASE	0xE000E100
 
 #endif	/* !_ARM_STM_STM32G0_H_ */
