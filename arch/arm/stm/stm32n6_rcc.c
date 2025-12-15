7f16a48335eb02634172e02f263feff22592fc42
Ruslan Bukin <br@bsdpad.com>
2025-12-15 19:23:46 +0000

    rework n6 test clock setup

--- arch/arm/stm/stm32n6_rcc.c
+++ arch/arm/stm/stm32n6_rcc.c
@@ -33,29 +33,79 @@
 #define	WR4(_sc, _reg, _val)	\
 	*(volatile uint32_t *)((_sc)->base + _reg) = _val
 
+/* Test setup */
+
 void
 stm32n6_rcc_pll1(struct stm32n6_rcc_softc *sc)
 {
 	uint32_t reg;
 
-	reg = PLL1CFGR1_PLL1SEL_HSE_CK;
-	reg |= 3 << PLL1CFGR1_PLLDIVM_S | 75 << PLL1CFGR1_PLLDIVN_S;
+	/* PLL1 1200 MHz */
+	reg = PLL1CFGR1_PLL1SEL_HSI_CK;
+	reg |= 4 << PLL1CFGR1_PLLDIVM_S | 75 << PLL1CFGR1_PLLDIVN_S;
 	WR4(sc, RCC_PLL1CFGR1, reg);
 
 	WR4(sc, RCC_CR, CR_LSI_ON | CR_HSI_ON | CR_HSE_ON | CR_PLL1_ON);
 
+	/* TODO: poll for PLL ready bit. */
+	udelay(10000);
+
+	/* DCMIPP 300 MHz */
 	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC17CFGR, reg);
 
+	/* CSI 20 MHz */
 	reg = ICxCFGR_ICxSEL_PLL1 | ((60 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC18CFGR, reg);
 
-	WR4(sc, RCC_CCIPR1, CCRPR1_DCMIPPSEL_IC17_CK);
+	/* CPU 600 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((2 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC1CFGR, reg);
 
-	reg = DIVENSR_ICxENS(17) | DIVENSR_ICxENS(18);
+	/* SYS buses 300 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC2CFGR, reg);
+
+	/* NPU 300 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC6CFGR, reg);
+
+	/* SRAM3456 150 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((8 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC11CFGR, reg);
+
+	/* LTDC 37.5 MHz: TODO check reqs */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((32 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC16CFGR, reg);
+
+	WR4(sc, RCC_CCIPR1, CCRPR1_DCMIPPSEL_IC17_CK);
+	WR4(sc, RCC_CCIPR4, CCIPR4_LTDCSEL_IC16_CK);
+
+	reg = 0;
+	reg |= DIVENSR_ICxENS(17); /* dcmipp */
+	reg |= DIVENSR_ICxENS(18); /* csi */
+	reg |= DIVENSR_ICxENS(1); /* cpu */
+	reg |= DIVENSR_ICxENS(2); /* ahb apb */
+	reg |= DIVENSR_ICxENS(6); /* npu */
+	reg |= DIVENSR_ICxENS(11); /* sram3456 */
+	reg |= DIVENSR_ICxENS(16); /* ltdc */
 	WR4(sc, RCC_DIVENSR, reg);
 
-	printf("RCC CR %x\n", RD4(sc, RCC_CR));
+	reg = 0;
+	reg |= 1 << CFGR2_HPRE_S;	/* div 2 so sysbuses 150 MHz */
+	reg |= 1 << CFGR2_TIMPRE_S;	/* div 2 so timers 150 MHz */
+	reg |= 0 << CFGR2_PPRE5_S;	/* div 1 apb5 */
+	reg |= 0 << CFGR2_PPRE4_S;	/* div 1 apb4 */
+	reg |= 0 << CFGR2_PPRE2_S;	/* div 1 apb2 */
+	reg |= 0 << CFGR2_PPRE1_S;	/* div 1 apb1 */
+	WR4(sc, RCC_CFGR2, reg);
+
+	/* Switch CPU and Sys Buses to the new clocks. */
+
+	reg = RD4(sc, RCC_CFGR1);
+	reg |= CFGR1_CPUSW_IC1_CK;
+	reg |= CFGR1_SYSSW_IC2_CK;
+	WR4(sc, RCC_CFGR1, reg);
 }
 
 void
