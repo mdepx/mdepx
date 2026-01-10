8447c6b3c8ba79a275aaeca2061f6dadd19c2e6c
Ruslan Bukin <br@bsdpad.com>
2026-01-10 16:05:21 +0000

    stm32n6 rcc: increase core/buses frequency.

--- arch/arm/stm/stm32n6_rcc.c
+++ arch/arm/stm/stm32n6_rcc.c
@@ -40,60 +40,74 @@ stm32n6_rcc_pll1(struct stm32n6_rcc_softc *sc)
 {
 	uint32_t reg;
 
-	/* PLL1 1200 MHz */
+	/* PLL1 800 MHz (64 x 25 / 2) */
 	reg = PLL1CFGR1_PLL1SEL_HSI_CK;
-	reg |= 4 << PLL1CFGR1_PLLDIVM_S | 75 << PLL1CFGR1_PLLDIVN_S;
+	reg |= 2 << PLL1CFGR1_PLL1DIVM_S | 25 << PLL1CFGR1_PLL1DIVN_S;
 	WR4(sc, RCC_PLL1CFGR1, reg);
 
-	WR4(sc, RCC_CR, CR_LSI_ON | CR_HSI_ON | CR_HSE_ON | CR_PLL1_ON);
+	/* PLL2 1000 Mhz */
+	reg = PLL2CFGR1_PLL2SEL_HSI_CK;
+	reg |= 8 << PLL2CFGR1_PLL2DIVM_S | 125 << PLL2CFGR1_PLL2DIVN_S;
+	WR4(sc, RCC_PLL2CFGR1, reg);
+
+	WR4(sc, RCC_CR, CR_LSI_ON | CR_HSI_ON | CR_HSE_ON | CR_PLL1_ON |
+	    CR_PLL2_ON);
 
 	/* TODO: poll for PLL ready bit. */
 	udelay(10000);
 
-	/* DCMIPP 300 MHz */
-	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
+	/* DCMIPP 333.3 MHz */
+	reg = ICxCFGR_ICxSEL_PLL2 | ((3 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC17CFGR, reg);
 
 	/* CSI 20 MHz */
-	reg = ICxCFGR_ICxSEL_PLL1 | ((60 - 1) << ICxCFGR_ICxINT_S);
+	reg = ICxCFGR_ICxSEL_PLL1 | ((40 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC18CFGR, reg);
 
-	/* CPU 600 MHz */
-	reg = ICxCFGR_ICxSEL_PLL1 | ((2 - 1) << ICxCFGR_ICxINT_S);
+	/* CPU 800 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((1 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC1CFGR, reg);
 
-	/* SYS buses 300 MHz */
-	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
+	/* SYS buses 400 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((2 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC2CFGR, reg);
 
-	/* NPU 300 MHz */
+	/* XSPI1 200 MHz */
 	reg = ICxCFGR_ICxSEL_PLL1 | ((4 - 1) << ICxCFGR_ICxINT_S);
+	WR4(sc, RCC_IC3CFGR, reg);
+
+	/* NPU 1000 MHz */
+	reg = ICxCFGR_ICxSEL_PLL2 | ((1 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC6CFGR, reg);
 
-	/* SRAM3456 150 MHz */
-	reg = ICxCFGR_ICxSEL_PLL1 | ((8 - 1) << ICxCFGR_ICxINT_S);
+	/* SRAM3456 800 MHz */
+	/* TODO: move to PLL3 for 900 MHz */
+	reg = ICxCFGR_ICxSEL_PLL1 | ((1 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC11CFGR, reg);
 
-	/* LTDC 37.5 MHz: TODO check reqs */
+	/* LTDC 25 MHz typ. */
 	reg = ICxCFGR_ICxSEL_PLL1 | ((32 - 1) << ICxCFGR_ICxINT_S);
 	WR4(sc, RCC_IC16CFGR, reg);
 
 	WR4(sc, RCC_CCIPR1, CCRPR1_DCMIPPSEL_IC17_CK);
 	WR4(sc, RCC_CCIPR4, CCIPR4_LTDCSEL_IC16_CK);
+	WR4(sc, RCC_CCIPR6, 2); //xspi1 IC3
+	WR4(sc, RCC_MISCENR, 1 << 3);
 
 	reg = 0;
 	reg |= DIVENSR_ICxENS(17); /* dcmipp */
 	reg |= DIVENSR_ICxENS(18); /* csi */
 	reg |= DIVENSR_ICxENS(1); /* cpu */
-	reg |= DIVENSR_ICxENS(2); /* ahb apb */
+	reg |= DIVENSR_ICxENS(2); /* sysb (ahb apb ...) */
+	reg |= DIVENSR_ICxENS(3); /* xspi1 */
 	reg |= DIVENSR_ICxENS(6); /* npu */
 	reg |= DIVENSR_ICxENS(11); /* sram3456 */
 	reg |= DIVENSR_ICxENS(16); /* ltdc */
 	WR4(sc, RCC_DIVENSR, reg);
 
 	reg = 0;
-	reg |= 1 << CFGR2_HPRE_S;	/* div 2 so sysbuses 150 MHz */
-	reg |= 1 << CFGR2_TIMPRE_S;	/* div 2 so timers 150 MHz */
+	reg |= 1 << CFGR2_HPRE_S;	/* div 2 so H sysbuses 200 MHz */
+	reg |= 1 << CFGR2_TIMPRE_S;	/* div 2 so timers 200 MHz */
 	reg |= 0 << CFGR2_PPRE5_S;	/* div 1 apb5 */
 	reg |= 0 << CFGR2_PPRE4_S;	/* div 1 apb4 */
 	reg |= 0 << CFGR2_PPRE2_S;	/* div 1 apb2 */
@@ -111,6 +125,7 @@ stm32n6_rcc_pll1(struct stm32n6_rcc_softc *sc)
 void
 stm32n6_rcc_setup(struct stm32n6_rcc_softc *sc, struct rcc_config *cfg)
 {
+	uint32_t reg;
 
 	/* Note this are Set registers (not write) */
 
@@ -127,6 +142,18 @@ stm32n6_rcc_setup(struct stm32n6_rcc_softc *sc, struct rcc_config *cfg)
 	WR4(sc, RCC_APB4HENSR, cfg->apb4henr);
 	WR4(sc, RCC_APB5ENSR, cfg->apb5enr);
 	WR4(sc, RCC_MEMENSR, cfg->memenr);
+
+	/* Reset XSPI1/2/M. */
+	reg = AHB5RSTR_XSPI1RST | AHB5RSTR_XSPI2RST | AHB5RSTR_XSPIMRST;
+	WR4(sc, RCC_AHB5RSTR, reg);
+	udelay(10);
+	WR4(sc, RCC_AHB5RSTR, 0);
+
+	/* Reset display/camera IP. */
+	reg = APB5RSTR_LTDCRST | APB5RSTR_DCMIPPRST | APB5RSTR_CSIRST;
+	WR4(sc, RCC_APB5RSTR, reg);
+	udelay(10);
+	WR4(sc, RCC_APB5RSTR, 0);
 }
 
 int
